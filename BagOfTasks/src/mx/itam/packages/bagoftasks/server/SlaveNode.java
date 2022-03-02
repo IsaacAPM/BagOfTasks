e081af2a48b244cf685bc5721629d1afaf4a59fb
IsaacAPM <isaacapimentelm@gmail.com>
2022-03-02 17:38:45 -0600

    executeTask agregado

--- BagOfTasks/src/mx/itam/packages/bagoftasks/server/SlaveNode.java
+++ BagOfTasks/src/mx/itam/packages/bagoftasks/server/SlaveNode.java
@@ -1,4 +1,40 @@
 package mx.itam.packages.bagoftasks.server;
 
-public class SlaveNode {
+import mx.itam.packages.bagoftasks.interfaces.Bioinformatics;
+import mx.itam.packages.bagoftasks.interfaces.DataMining;
+import mx.itam.packages.bagoftasks.interfaces.ImageProcessing;
+import mx.itam.packages.bagoftasks.serializableobjects.Task;
+
+public class SlaveNode implements Bioinformatics, DataMining, ImageProcessing {
+
+    public Task executeBioTask(Task task){
+        try {
+            Thread.sleep(task.getLength());
+            task.setOutput("Tarea de bioinformatica terminada");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return task;
+    }
+
+    public Task executeDataTask(Task task){
+        try {
+            Thread.sleep(task.getLength());
+            task.setOutput("Tarea de minerìa terminada");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return task;
+    }
+
+    public Task executeImageTask(Task task){
+        try {
+            Thread.sleep(task.getLength());
+            task.setOutput("Tarea de procesamiento de imagenes terminada");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return task;
+    }
+
 }
