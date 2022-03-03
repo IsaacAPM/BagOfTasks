0c4886ea2e5ffb78e09570fe1c400ec487935339
IsaacAPM <isaacapimentelm@gmail.com>
2022-03-02 19:05:25 -0600

    todo listo

--- BagOfTasks/src/mx/itam/packages/bagoftasks/client/MasterNode.java
+++ BagOfTasks/src/mx/itam/packages/bagoftasks/client/MasterNode.java
@@ -1,11 +1,16 @@
 package mx.itam.packages.bagoftasks.client;
 
+import mx.itam.packages.bagoftasks.interfaces.Bioinformatics;
+import mx.itam.packages.bagoftasks.interfaces.DataMining;
+import mx.itam.packages.bagoftasks.interfaces.ImageProcessing;
 import mx.itam.packages.bagoftasks.serializableobjects.Task;
+import mx.itam.packages.bagoftasks.server.SlaveNode;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class MasterNode {
 
@@ -19,7 +24,7 @@ public class MasterNode {
 
         String name = "BagOfTasks";
         try {
-            Registry registry = LocateRegistry.getRegistry("148.205.133.194");
+            Registry registry = LocateRegistry.getRegistry("localhost");
 
             Task[] imageTasks = new Task[10];
             Task[] miningTasks = new Task[20];
@@ -66,11 +71,62 @@ public class MasterNode {
                 }
             }
 
+            MyThread bioNodo = new MyThread(registry,bioTasks);
+            MyThread minNodo = new MyThread(registry,miningTasks);
+            MyThread imgNodo = new MyThread(registry,imageTasks);
+
+            long start = System.currentTimeMillis();
+
+            bioNodo.start();
+            minNodo.start();
+            imgNodo.start();
+
+            bioNodo.join();
+            minNodo.join();
+            imgNodo.join();
+
+            long end = System.currentTimeMillis();
+            System.out.println(end-start);
+
         } catch (RemoteException e) {
             e.printStackTrace();
-        } catch (NotBoundException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    private static class MyThread extends Thread{
+        private Registry registry;
+        private Task[] bag;
+
+        public MyThread(Registry registry,Task[] bag){
+            this.registry = registry;
+            this.bag = bag;
+        }
+
+        public void run(){
+            try {
+                SlaveNode slave = (SlaveNode) this.registry.lookup(this.bag[0].getRequirementId());
+
+                for(Task task:this.bag) {
+                    if (task.getRequirementId().equals("Imagenes")) {
+                        slave.executeImageTask(task);
+                    } else if (task.getRequirementId().equals("Mineria")) {
+                        slave.executeDataTask(task);
+                    } else if (task.getRequirementId().equals("Bioinformatica")) {
+                        slave.executeBioTask(task);
+                    } else {
+                        System.out.println("Tipo de servicio invàlido");
+                    }
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
+
+
--- BagOfTasks/src/mx/itam/packages/bagoftasks/server/Deployer.java
+++ BagOfTasks/src/mx/itam/packages/bagoftasks/server/Deployer.java
@@ -1,9 +1,40 @@
 package mx.itam.packages.bagoftasks.server;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Deployer {
 
     public static void main(String[] args) {
+        System.setProperty("java.security.policy",
+                "src/mx/itam/packages/bagoftasks/server/server.policy");
 
+        if(System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+
+            String serverAddres = "localhost";
+            System.setProperty("java.rmi.server.hostname",serverAddres);
+
+            LocateRegistry.createRegistry(1099);
+
+            Registry registry = LocateRegistry.getRegistry();
+
+            SlaveNode slaveImg = new SlaveNode("Imagenes",registry);
+            SlaveNode slaveMin = new SlaveNode("Mineria",registry);
+            SlaveNode slaveBio = new SlaveNode("Bioinformatica",registry);
+
+            slaveImg.despliegue();
+            slaveMin.despliegue();
+            slaveBio.despliegue();
+
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
 }
